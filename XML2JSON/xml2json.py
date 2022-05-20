4271799c7d3e18f77b95da6908cc6297f71fa696
SmashinFries <smashinfries1@gmail.com>
2022-05-19 19:24:40 -0500

    added version menu

--- XML2JSON/xml2json.py
+++ XML2JSON/xml2json.py
@@ -1,13 +1,52 @@
-from XML2JSON.kanjiconvert import version_menu
-from helpers import clearConsole, menu_title, select_input
+from XML2JSON.kanjiconvert import writeJSON, writeJSON2
+from XML2JSON.vocabconvert import writeVocabJSON
+from helpers import clearConsole, select_input
 from termcolor import colored
+
+def version_menu(dic:str):
+    clearConsole()
+    print(colored("Select Type of JSON", on_color='on_green'))
+    print(colored("\nDictionary: " + dic, color="cyan"))
+    if dic == 'Kanji':
+        print(f"""
+        {colored('1', color='magenta')} : Objects stored in a big list
+        {colored('2', color='green')} : No list, each object is keyed by kanji literal
+        {colored('0', color='red')} : Go Back"""
+        )
+        choice = select_input()
+        if choice == '1':
+            print(colored("Parsing XML!\n", on_color='on_green'))
+            writeJSON()
+            print(colored("\nSaved at: ./json/", on_color="on_cyan"))
+            print(colored("またね！", on_color="on_red"))
+        elif choice == '2':
+            print(colored("Parsing XML!\n", on_color='on_green'))
+            writeJSON2()
+            print(colored("\nSaved at: ./json/", on_color="on_cyan"))
+            print(colored("またね！", on_color="on_red"))
+        elif choice == '0':
+            exit()
+    
+    elif dic == 'Vocab':
+        print(f"""
+        {colored('1', color='magenta')} : Objects stored in a big list
+        {colored('0', color='red')} : Go Back"""
+        )
+        choice = select_input()
+        if choice == '1':
+            print(colored("Parsing XML!\n", on_color='on_green'))
+            writeVocabJSON()
+            print(colored("\nSaved at: ./user_data/json/", on_color="on_cyan"))
+            print(colored("またね！", on_color="on_red"))
+        elif choice == '0':
+            exit()
 
 def xml2json(avaiable):
     option1 = f"{colored('1', color='magenta')} : Kanji Dictionary"
     option2 = f"{colored('2', color='green')} : Vocabulary Dictionary"
-
+    
+    clearConsole()
     while True:
-        clearConsole()
         print(colored("Use Kanji or Vocab Dictionary?", on_color='on_green'))
         print(f"""
         {option1 if avaiable['kanji'] else 'Please add kanjidic2.xml to /user_data/xml/ folder'}
@@ -19,7 +58,7 @@ def xml2json(avaiable):
         if selectedDictionary == '1':
             version_menu(dic='Kanji')
         elif selectedDictionary == '2':
-            exit()
+            version_menu(dic='Vocab')
         elif selectedDictionary == '0':
             clearConsole()
             return
